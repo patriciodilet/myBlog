2cd1096d7bc3b4ab06d91d89a9b1e77b66f97e76
AIEP\Patricio Diaz <Patricio Daz>
2022-08-15 13:23:50 -0400

    interface implemented

---
+++ Data/Repository/Repository.cs
@@ -0,0 +1,45 @@
+﻿using myBlog.Interfaces;
+using myBlog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace myBlog.Data.Repository
+{
+    public class Repository : IRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void AddPost(Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Post> GetAllPosts()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Post GetPost(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemovePost(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdatePost(Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using myBlog.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace myBlog.Interfaces
+{
+    public interface IRepository
+    {
+        Post GetPost(int id);
+        List<Post> GetAllPosts();
+        void AddPost(Post post);
+        void UpdatePost(Post post);
+        void RemovePost(int id);
+        Task<bool> SaveChangesAsync();
+    }
+}
