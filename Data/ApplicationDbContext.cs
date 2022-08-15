f4326dc483146c7d8079c5432c57a0ec3a9b09d7
AIEP\Patricio Diaz <Patricio Daz>
2022-08-15 13:11:34 -0400

    creation of models and migrations

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using myBlog.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,6 @@ namespace myBlog.Data
             : base(options)
         {
         }
+        public DbSet<Post> Posts { get; set; }
     }
 }
